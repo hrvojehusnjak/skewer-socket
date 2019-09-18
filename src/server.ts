33deaf9d3f3dae0b9efe852bb0173532d0994a8e
hhusnjak <hrc099@gmail.com>
2019-09-18 09:49:52 +0200

    Basic Socket.io setup
    Add axios dependency
    Emit lorem ipsum text via socket

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,43 @@
-import express from 'express'
 import http from 'http'
+import querystring from 'querystring'
+
+import axios, { AxiosPromise, AxiosResponse } from 'axios'
+import express from 'express'
 import socketio from 'socket.io'
+
+const STREAM_INTERVAL = 20000
+const baseUrl = 'https://baconipsum.com/api/?'
+const types = ['all-meat', 'meat-and-filler']
+
+function randomizeUrlParams (baseUrl: string, types: string[]): string {
+  const type: string = types[Math.round(Math.random())]
+  const sentences: number = Math.floor(Math.random() * 100)
+  return baseUrl + querystring.stringify({ type, sentences })
+}
+
+async function getIpsumTexts (link: string): Promise<string[]> {
+  const textPromise: AxiosPromise = axios.get(link)
+  const baconIpsumRes: AxiosResponse[] = await axios.all([textPromise, textPromise])
+  return baconIpsumRes.map((res) => res.data[0])
+}
+
+async function streamComparisons (socket: SocketIO.Socket, ticker: any) {
+  const link: string = randomizeUrlParams(baseUrl, types)
+  const texts: string[] = await getIpsumTexts(link)
+  console.log('link', link)
+  console.log(texts[0])
+  socket.emit('ticker', texts)
+}
+
+async function trackTicker (socket: SocketIO.Socket, ticker: any) {
+  await streamComparisons(socket, ticker)
+  const timer = setInterval(async () => {
+    await streamComparisons(socket, ticker)
+  }, STREAM_INTERVAL)
+  socket.on('disconnect', () => {
+    clearInterval(timer)
+  })
+}
 
 const app = express()
 app.set('port', process.env.PORT || 3800)
@@ -15,4 +52,8 @@ app.get('/', (req, res) => {
 
 io.sockets.on('connection', (socket) => {
   console.log('socket connected')
+  socket.on('ticker', async (ticker) => {
+    console.log('woohoo ticker', ticker)
+    await trackTicker(socket, ticker)
+  })
 })
